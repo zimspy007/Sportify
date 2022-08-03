68870422cd610aec8ff9e753cf74952f1647413f
Van Lee <vanleechigwada@gmail.com>
2022-08-03 14:18:28 +0200

    Font & Event Item entity

---
+++ app/src/main/java/com/vanlee/sportify/database/objectbox/entities/DbEventItem.kt
@@ -0,0 +1,15 @@
+package com.vanlee.sportify.database.objectbox.entities
+
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+
+@Entity
+data class DbEventItem(
+    @Id
+    var id: Long = 0,
+    var title: String? = null,
+    var subTitle: String? = null,
+    var rawTime: String? = null,
+    var formattedDate: String? = null,
+    var formattedTime: String? = null
+)
